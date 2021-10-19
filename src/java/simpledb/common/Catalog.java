eca2a5746c99be8c2ea10cc1055cb11f34dafd47
ShuangShu <1103725164@qq.com>
2021-10-19 14:29:16 +0800

    exersise finished

---
+++ src/java/simpledb/common/Catalog.java
@@ -0,0 +1,232 @@
+package simpledb.common;
+
+import simpledb.common.Type;
+import simpledb.storage.DbFile;
+import simpledb.storage.HeapFile;
+import simpledb.storage.TupleDesc;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * The Catalog keeps track of all available tables in the database and their
+ * associated schemas.
+ * For now, this is a stub catalog that must be populated with tables by a
+ * user program before it can be used -- eventually, this should be converted
+ * to a catalog that reads a catalog table from disk.
+ * 
+ * @Threadsafe
+ */
+public class Catalog {
+
+    class TableIdIterator implements Iterator<Integer>{
+        private int index;
+        public TableIdIterator(){
+            this.index=0;
+        }
+        @Override
+        public boolean hasNext() {
+            if(index<tupleDescs.size())
+                return true;
+            else
+                return false;
+        }
+
+        @Override
+        public Integer next() {
+            index++;
+            return DbFiles.get(this.index).getId();
+        }
+    }
+    /**
+     * Constructor.
+     * Creates a new, empty catalog.
+     */
+    public Catalog() {
+        // some code goes here
+    }
+
+    /**
+     * Add a new table to the catalog.
+     * This table's contents are stored in the specified DbFile.
+     * @param file the contents of the table to add;  file.getId() is the identfier of
+     *    this file/tupledesc param for the calls getTupleDesc and getFile
+     * @param name the name of the table -- may be an empty string.  May not be null.  If a name
+     * conflict exists, use the last table to be added as the table for a given name.
+     * @param pkeyField the name of the primary key field
+     */
+    public void addTable(DbFile file, String name, String pkeyField) {
+        // some code goes here
+        boolean isAdded=false;
+        for(int i=0;i<this.DbFiles.size();++i) {
+            int id=file.getId();
+            if (Objects.equals(name, this.names.get(i))||id==this.DbFiles.get(i).getId()) {
+                //更新第i项的数据
+                this.DbFiles.set(i, file);
+                this.names.set(i, name);
+                this.primaryKeys.set(i, pkeyField);
+                this.tupleDescs.set(i, file.getTupleDesc());
+                isAdded=true;
+            }
+        }
+        if(isAdded==false){
+            //要考虑name和fileId的冲突，若有冲突则将相关数据更新
+            this.tupleDescs.add(file.getTupleDesc());
+            this.primaryKeys.add(pkeyField);
+            this.names.add(name);
+            this.DbFiles.add(file);
+        }
+    }
+
+    public void addTable(DbFile file, String name) {
+        addTable(file, name, "");
+    }
+
+    /**
+     * Add a new table to the catalog.
+     * This table has tuples formatted using the specified TupleDesc and its
+     * contents are stored in the specified DbFile.
+     * @param file the contents of the table to add;  file.getId() is the identfier of
+     *    this file/tupledesc param for the calls getTupleDesc and getFile
+     */
+    public void addTable(DbFile file) {
+        addTable(file, (UUID.randomUUID()).toString());
+    }
+
+    /**
+     * Return the id of the table with a specified name,
+     * @throws NoSuchElementException if the table doesn't exist
+     */
+    public int getTableId(String name) throws NoSuchElementException {
+        // some code goes here
+        for(int i=0;i<this.DbFiles.size();++i){
+            if(Objects.equals(name, this.names.get(i)))
+                return this.DbFiles.get(i).getId();
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * Returns the tuple descriptor (schema) of the specified table
+     * @param tableid The id of the table, as specified by the DbFile.getId()
+     *     function passed to addTable
+     * @throws NoSuchElementException if the table doesn't exist
+     */
+    public TupleDesc getTupleDesc(int tableid) throws NoSuchElementException {
+        // some code goes here
+        for(int i=0;i<this.DbFiles.size();++i){
+            if(this.DbFiles.get(i).getId()==tableid)
+                return this.tupleDescs.get(i);
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * Returns the DbFile that can be used to read the contents of the
+     * specified table.
+     * @param tableid The id of the table, as specified by the DbFile.getId()
+     *     function passed to addTable
+     */
+    public DbFile getDatabaseFile(int tableid) throws NoSuchElementException {
+        for(int i=0;i<this.DbFiles.size();++i){
+            if(this.DbFiles.get(i).getId()==tableid)
+                return this.DbFiles.get(i);
+        }
+        throw new NoSuchElementException();
+    }
+
+    public String getPrimaryKey(int tableid) {
+        // some code goes here
+        for(int i=0;i<this.DbFiles.size();++i){
+            if(this.DbFiles.get(i).getId()==tableid)
+                return this.primaryKeys.get(i);
+        }
+        throw new NoSuchElementException();
+    }
+
+    public Iterator<Integer> tableIdIterator() {
+        // some code goes here
+        return new TableIdIterator();
+    }
+
+    public String getTableName(int id) {
+        // some code goes here
+        for(int i=0;i<this.DbFiles.size();++i){
+            if(this.DbFiles.get(i).getId()==id)
+                return this.names.get(i);
+        }
+        throw new NoSuchElementException();
+    }
+    
+    /** Delete all tables from the catalog */
+    public void clear() {
+        // some code goes here
+        this.primaryKeys=new ArrayList<>();
+        this.DbFiles=new ArrayList<>();
+        this.tupleDescs=new ArrayList<>();
+        this.names=new ArrayList<>();
+    }
+    
+    /**
+     * Reads the schema from a file and creates the appropriate tables in the database.
+     * @param catalogFile
+     */
+    public void loadSchema(String catalogFile) {
+        String line = "";
+        String baseFolder=new File(new File(catalogFile).getAbsolutePath()).getParent();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(catalogFile));
+            
+            while ((line = br.readLine()) != null) {
+                //assume line is of the format name (field type, field type, ...)
+                String name = line.substring(0, line.indexOf("(")).trim();
+                //System.out.println("TABLE NAME: " + name);
+                String fields = line.substring(line.indexOf("(") + 1, line.indexOf(")")).trim();
+                String[] els = fields.split(",");
+                ArrayList<String> names = new ArrayList<>();
+                ArrayList<Type> types = new ArrayList<>();
+                String primaryKey = "";
+                for (String e : els) {
+                    String[] els2 = e.trim().split(" ");
+                    names.add(els2[0].trim());
+                    if (els2[1].trim().equalsIgnoreCase("int"))
+                        types.add(Type.INT_TYPE);
+                    else if (els2[1].trim().equalsIgnoreCase("string"))
+                        types.add(Type.STRING_TYPE);
+                    else {
+                        System.out.println("Unknown type " + els2[1]);
+                        System.exit(0);
+                    }
+                    if (els2.length == 3) {
+                        if (els2[2].trim().equals("pk"))
+                            primaryKey = els2[0].trim();
+                        else {
+                            System.out.println("Unknown annotation " + els2[2]);
+                            System.exit(0);
+                        }
+                    }
+                }
+                Type[] typeAr = types.toArray(new Type[0]);
+                String[] namesAr = names.toArray(new String[0]);
+                TupleDesc t = new TupleDesc(typeAr, namesAr);
+                HeapFile tabHf = new HeapFile(new File(baseFolder+"/"+name + ".dat"), t);
+                addTable(tabHf,name,primaryKey);
+                System.out.println("Added table : " + name + " with schema " + t);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println ("Invalid catalog entry : " + line);
+            System.exit(0);
+        }
+    }
+
+    //类域，存储了一组tupleDesc，每个tupleDesc与DbFiles中的一个元素对应
+    private ArrayList<TupleDesc> tupleDescs=new ArrayList<>();
+    private ArrayList<DbFile> DbFiles=new ArrayList<>();
+    private ArrayList<String> names=new ArrayList<>();
+    private ArrayList<String> primaryKeys=new ArrayList<>();
+}
+
---
+++ src/java/simpledb/storage/TupleDesc.java
@@ -0,0 +1,247 @@
+package simpledb.storage;
+
+import simpledb.common.Type;
+
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * TupleDesc describes the schema of a tuple.
+ */
+public class TupleDesc implements Serializable {
+
+    /**
+     * A help class to facilitate organizing the information of each field
+     * 一个帮助类，便于组织每个域的信息
+     * */
+    public static class TDItem implements Serializable {
+        /*
+        * 一个内部类，用于描述某一个Tuple属性的属性（即域名和域类型）
+        *
+        * */
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * The type of the field
+         * */
+        public final Type fieldType;
+        
+        /**
+         * The name of the field
+         * */
+        public final String fieldName;
+
+        public TDItem(Type t, String n) {
+            this.fieldName = n;
+            this.fieldType = t;
+        }
+
+        public String toString() {
+            return fieldName + "(" + fieldType + ")";
+        }
+    }
+
+    /**
+     * @return
+     *        一个返回Iterator<TDItem> 的函数，用以迭代整个TupleDesc
+     *        An iterator which iterates over all the field TDItems
+     *        that are included in this TupleDesc
+     * */
+    public Iterator<TDItem> iterator() {
+        // some code goes here
+        return this.TdList.iterator();
+    }
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create a new TupleDesc with typeAr.length fields with fields of the
+     * specified types, with associated named fields.
+     * 有typeAr和fieldAr作为参数的构造函数
+     *
+     * 用typeAr(ray).length字段创建一个新的TupleDesc，其中有指定类型的字段，有相关的命名字段。
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     * @param fieldAr
+     *            array specifying the names of the fields.
+     *
+     *            Note that names may be null.
+     */
+    public TupleDesc(Type[] typeAr, String[] fieldAr) {
+        // some code goes here_OK
+        this.TdList=new ArrayList<>();
+        for(int i=0;i<typeAr.length;++i)
+            this.TdList.add(new TDItem(typeAr[i], fieldAr[i]));
+    }
+
+    /**
+     * Constructor. Create a new tuple desc with typeAr.length fields with
+     * fields of the specified types, with anonymous (unnamed) fields.
+     * 仅有typeAr的为参数的构造函数
+     *
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     */
+    public TupleDesc(Type[] typeAr) {
+        // some code goes here
+        this.TdList=new ArrayList<>();
+        for (Type type : typeAr) this.TdList.add(new TDItem(type, null));
+    }
+
+    /**
+     * @return the number of fields in this TupleDesc
+     */
+    public int numFields() {
+        // some code goes here
+        return this.TdList.size();
+    }
+
+    /**
+     * Gets the (possibly null) field name of the ith field of this TupleDesc.
+     * 
+     * @param i
+     *            index of the field name to return. It must be a valid index.
+     * @return the name of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public String getFieldName(int i) throws NoSuchElementException {
+        // some code goes here
+        return this.TdList.get(i).fieldName;
+    }
+
+    /**
+     * Gets the type of the ith field of this TupleDesc.
+     * 
+     * @param i
+     *            The index of the field to get the type of. It must be a valid
+     *            index.
+     * @return the type of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public Type getFieldType(int i) throws NoSuchElementException {
+        // some code goes here
+        return this.TdList.get(i).fieldType;
+    }
+
+    /**
+     * Find the index of the field with a given name.
+     * 
+     * @param name
+     *            name of the field.
+     * @return the index of the field that is first to have the given name.
+     * @throws NoSuchElementException
+     *             if no field with a matching name is found.
+     */
+    public int fieldNameToIndex(String name) throws NoSuchElementException {
+        Iterator<TDItem> iterator=this.iterator();
+        int i=0;
+        while (iterator.hasNext()){
+            if(Objects.equals(iterator.next().fieldName, name))
+                return i;
+            i++;
+        }
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * @return The size (in bytes) of tuples corresponding to this TupleDesc.
+     *         Note that tuples from a given TupleDesc are of a fixed size.
+     */
+    public int getSize() {
+        // some code goes here
+        int size=0;
+        Iterator<TDItem> iter=this.iterator();
+        while (iter.hasNext())
+            size+=iter.next().fieldType.getLen();
+        return size;
+    }
+
+    /**
+     * Merge two TupleDescs into one, with td1.numFields + td2.numFields fields,
+     * with the first td1.numFields coming from td1 and the remaining from td2.
+     * 
+     * @param td1
+     *            The TupleDesc with the first fields of the new TupleDesc
+     * @param td2
+     *            The TupleDesc with the last fields of the TupleDesc
+     * @return the new TupleDesc
+     */
+    public static TupleDesc merge(TupleDesc td1, TupleDesc td2) {
+        // some code goes here
+        int size1= td1.numFields();
+        int size2= td2.numFields();
+        String [] tempName=new String[size1+size2];
+        Type[] tempType=new Type[size1+size2];
+        for(int i=0;i<size1;++i) {
+            tempName[i] = td1.TdList.get(i).fieldName;
+            tempType[i]= td1.TdList.get(i).fieldType;
+        }
+        for(int i=size1;i<size1+size2;++i){
+            tempName[i] = td2.TdList.get(i-size1).fieldName;
+            tempType[i]= td2.TdList.get(i-size1).fieldType;
+        }
+        return new TupleDesc(tempType, tempName);
+    }
+
+    /**
+     * Compares the specified object with this TupleDesc for equality. Two
+     * TupleDescs are considered equal if they have the same number of items
+     * and if the i-th type in this TupleDesc is equal to the i-th type in o
+     * for every i.
+     * 
+     * @param o
+     *            the Object to be compared for equality with this TupleDesc.
+     * @return true if the object is equal to this TupleDesc.
+     */
+
+    public boolean equals(Object o) {
+        // some code goes here
+        if(o==this)
+            //若o为这个对象本身的的引用
+            return true;
+        if(!(o instanceof TupleDesc))
+            return false;
+        TupleDesc temp=(TupleDesc)o;
+        if(this.numFields()!=temp.numFields())
+            return false;
+
+        Iterator<TDItem> iterator1=temp.iterator();
+        Iterator<TDItem> iterator2=temp.iterator();
+
+        while (iterator1.hasNext()){
+            TDItem temp1=iterator1.next();
+            TDItem temp2=iterator2.next();
+            if(temp1.fieldType!=temp2.fieldType|| !Objects.equals(temp1.fieldName, temp2.fieldName))
+                return false;
+        }
+        return true;
+    }
+
+    public int hashCode() {
+        // If you want to use TupleDesc as keys for HashMap, implement this so
+        // that equal objects have equals hashCode() results
+        return this.TdList.hashCode();
+    }
+
+    /**
+     * Returns a String describing this descriptor. It should be of the form
+     * "fieldType[0](fieldName[0]), ..., fieldType[M](fieldName[M])", although
+     * the exact format does not matter.
+     * 
+     * @return String describing this descriptor.
+     */
+    public String toString() {
+        // some code goes here
+        StringBuilder result= new StringBuilder();
+        for(int i=0;i<this.TdList.size()-1;++i)
+            result.append(this.TdList.get(i).toString()).append(",");
+        result.append(this.TdList.get(this.TdList.size() - 1).toString());
+        return result.toString();
+    }
+    //Tuple类域，一个TdItem的array
+    private final ArrayList<TDItem> TdList;
+}
